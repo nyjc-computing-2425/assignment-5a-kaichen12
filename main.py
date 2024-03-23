af271de1232766da39163d03a25bb0aa6687c842
kaichen12 <160604150+kaichen12@users.noreply.github.com>
2024-03-23 14:14:38 +0000

    final submission

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def to_hms(seconds: int) -> list:
     """
     Converts seconds to hours, minutes, and seconds, and returns it as a list.
@@ -21,5 +22,9 @@ def to_hms(seconds: int) -> list:
     [1, 59, 59]
     """
     # Type your code below
-    pass
-
+    if type(seconds) != int or seconds < 0:
+        print('Unsupported input type.')
+    else:
+        hours, remain_sec = divmod(seconds,3600)
+        mins, secs = divmod(remain_sec,60)
+        return [hours, mins, secs]
